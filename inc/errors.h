f0955ff8bd6145f898a0412ae30d734343a2dcd8
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-01-14 11:07:13 +0100

    create errors.h

--- inc/errors.h
+++ inc/errors.h
@@ -1,27 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   errors.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/09 10:07:09 by cpalusze          #+#    #+#             */
-/*   Updated: 2023/01/14 10:00:36 by cpalusze         ###   ########.fr       */
+/*   Created: 2023/01/14 10:00:02 by cpalusze          #+#    #+#             */
+/*   Updated: 2023/01/14 10:00:21 by cpalusze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-
-// Note: printf can is authorized
-# include <stdio.h>
-
-// Note: readline includes - all useful ?
-# include <readline/readline.h>
-# include <readline/history.h>
-
-# include "libft.h"
-
-# define PROMPT	"minishell-0.1$ "
+#ifndef ERRORS_H
+# define ERROR_H
 
 #endif
