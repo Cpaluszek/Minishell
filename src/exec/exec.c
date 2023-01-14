4f7deb0fdf1d029cb0cb932519a9d230d691028e
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-01-14 12:21:00 +0100

    exec_cmd prototype

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,13 +6,13 @@
 /*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 13:00:17 by cpalusze          #+#    #+#             */
-/*   Updated: 2023/01/14 11:46:40 by cpalusze         ###   ########.fr       */
+/*   Updated: 2023/01/14 12:20:44 by cpalusze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "structs.h"
 
-int	exec_start(s_block **block)
+int	exec_start(t_block **block)
 {
 	// Foreach block
 	while (*block != NULL)
@@ -29,7 +29,7 @@ int	exec_start(s_block **block)
 
 // For pipes: use minimal amount of pipes
 // create pipe in child then close it ASAP
-int	exec_block(s_block **block)
+int	exec_block(t_block **block)
 {
 	// Foreach token
 	while (block->token_list)
@@ -42,4 +42,12 @@ int	exec_block(s_block **block)
 		block->token_list = block->token_list->next;
 	}
 	// Set block exit status
+}
+
+int exec_cmd(t_token **token)
+{
+	// Fork
+		// Manage redirections
+			// open files
+		// exec
 }
