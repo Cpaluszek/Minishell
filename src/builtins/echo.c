705a47c556e9033a20a82f36050be97fee9b1a69
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-01-18 11:15:52 +0100

    builtin: echo draft

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 12:57:33 by cpalusze          #+#    #+#             */
-/*   Updated: 2023/01/17 16:25:49 by cpalusze         ###   ########.fr       */
+/*   Updated: 2023/01/18 10:15:37 by cpalusze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,53 @@
 // Option -n: print without trailing newline
 // The echo utility exits 0 on success, and >0 if an error occurs.
 #include "structs.h"
+#include "libft.h"
+#include <unistd.h>
 #define OPTION "\n"
 
+static int	echo_option(t_token *token, int *i);
+
+// Todo: fork + redirect
 int	ft_echo(t_token *token)
 {
 	int	i;
 	int	new_line;
 
 	i = 1;
-	new_line = 1;
-	// Skip all "-n" and "-nnnnn"
-	while (ft_strncmp("-n", OPTION, ft_strlen(OPTION)))
+	new_line = echo_option(token, &i);
+	while (token->cmd[i])
 	{
-		new_line = 0;
+		ft_putstr_fd(token->cmd[i], STDOUT_FILENO);
+		if (token->cmd[i + 1])
+			ft_putstr_fd(" ", STDOUT_FILENO);
 		i++;
-	}		
+	}
+	if (new_line)
+		ft_putstr_fd("\n", STDOUT_FILENO);
+}
+
+// Skip all "-n" and "-nnnnn"
+static int	echo_option(t_token *token, int *i)
+{
+	int	new_line;
+	int	j;
+
+	new_line = 1;
+	j = 0;
+	while (ft_strncmp(token->cmd[*i], OPTION, ft_strlen(OPTION)) == 0)
+	{
+		j += ft_strlen(OPTION);
+		while (token->cmd[*i][j])
+		{
+			if (token->cmd[*i][j] != 'n')
+			{
+				if (*i == 1)
+					return (1);
+				else
+					return (new_line);
+			}	
+		}
+		(*i)++;
+	}
+	return (new_line);
 }
