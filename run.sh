5cf0d8ff068ae30aceb6269e295f82b120f22a66
Cpaluszek <paluszek.clement@gmail.com>
2023-01-20 12:03:03 +0100

    update valgrind flags

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-valgrind --show-leak-kinds=all --leak-check=full --track-fds=yes --trace-children=yes --verbose --show-mismatched-frees=yes --read-var-info=yes --suppressions=valgrind_ignore_leaks.txt --log-file=valgrind-out.txt ./minishell
+valgrind --show-leak-kinds=all --leak-check=full --track-fds=yes --verbose --show-mismatched-frees=yes --read-var-info=yes --suppressions=valgrind_ignore_leaks.txt --log-file=valgrind-out.txt ./minishell
+#valgrind --show-leak-kinds=all --leak-check=full --track-fds=yes --trace-children=yes --verbose --show-mismatched-frees=yes --read-var-info=yes --suppressions=valgrind_ignore_leaks.txt --log-file=valgrind-out.txt ./minishell
