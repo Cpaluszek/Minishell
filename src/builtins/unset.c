1dfed79db7dde4e5eff5d1d6a99db39472afbcf9
Cpaluszek <paluszek.clement@gmail.com>
2023-01-21 18:26:34 +0100

    remove comments from unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,13 +6,10 @@
 /*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 12:57:48 by cpalusze          #+#    #+#             */
-/*   Updated: 2023/01/18 16:52:26 by cpalusze         ###   ########.fr       */
+/*   Updated: 2023/01/21 18:26:15 by cpalusze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Remove a variable from environment
-// Can remove multiples variables at once
-	// unset a b c
 #include "structs.h"
 #include "exec.h"
 #include <stdio.h>
